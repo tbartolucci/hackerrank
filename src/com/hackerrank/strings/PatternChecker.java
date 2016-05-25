180d7bb4430905aca58db84eb712229f19712fac
tbartolucci <tbartolucci@gmail.com>
2016-05-24 22:41:29 -0400

    Moving on to pattern matching.

---
+++ src/com/hackerrank/strings/PatternChecker.java
@@ -0,0 +1,17 @@
+package com.hackerrank.strings;
+
+import java.util.Scanner;
+
+/**
+ * Created by Tom on 5/24/2016.
+ */
+public class PatternChecker {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String pattern = in.nextLine();
+            //Write your code
+        }
+    }
+}
--- src/com/hackerrank/strings/Token.java
+++ src/com/hackerrank/strings/Token.java
@@ -1,11 +1,35 @@
 package com.hackerrank.strings;
 
-/**
- * Created by Tom on 5/24/2016.
- */
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Token {
 
-    public static void main(String[] args){
+
+    public static void main(String[] args)
+    {
+
+
+        //Scanner scan = new Scanner(System.in);
+        //String s=scan.nextLine();
+        //Complete the code
+
+        String s = "He is a very very good boy, isn't he?";
+
+        int numberOfTokens = 0;
+        ArrayList<String> list = new ArrayList<String>();
+        StringTokenizer st = new StringTokenizer(s," !,?._'@");
+        while (st.hasMoreTokens()) {
+            numberOfTokens++;
+            list.add(st.nextToken());
+        }
+
+        System.out.println(numberOfTokens);
+        for (String string : list) {
+            System.out.println(string);
+        }
 
     }
 }
