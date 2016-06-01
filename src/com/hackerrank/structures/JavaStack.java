20f09e5416ec1aabac2e02be2795724bda6fd50a
Tom Bartolucci <tbartolucci@billtrust.com>
2016-06-01 13:54:25 -0400

    Worked for initial test cases.  But failing for second set.

--- src/com/hackerrank/structures/JavaStack.java
+++ src/com/hackerrank/structures/JavaStack.java
@@ -1,21 +1,92 @@
 package com.hackerrank.structures;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
+/**
+ * ({}[])
+(({()})))
+({(){}()})()({(){}()})(){()}
+{}()))(()()({}}{}
+}}}}
+))))
+{{{
+(((
+[]{}(){()}((())){{{}}}{()()}{{}{}}
+[[]][][]
 
+true
+false
+true
+false
+false
+false
+false
+false
+true
+true
+ */
 public class JavaStack {
 
 	public static void main(String[] args) {
+		ArrayList<String> inputs = new ArrayList<String>();
+		inputs.add("({}[])");
+//		inputs.add("(({()})))");
+//		inputs.add("({(){}()})()({(){}()})(){()}(");
+//		inputs.add("{}()))(()()({}}{}");
+//		inputs.add("}}}}");
+//		inputs.add("))))");
+//		inputs.add("{{{");
+//		inputs.add("(((");
+//		inputs.add("[]{}(){()}((())){{{}}}{()()}{{}{}}");
+//		inputs.add("[[]][][]");
+	
 		Scanner sc = new Scanner(System.in);
-		//Map<String, Integer> stack = new HashMap<String, Integer>();
-		Stack<String> stack = new Stack<String>();
-		while (sc.hasNext()) {
-			String input = sc.next();
-			stack.push(input);
+		for ( int i = 0; i < inputs.size(); i++ ){
+		//while (sc.hasNext()) {
+			//String input = sc.next();
+			String input = inputs.get(i);
+			validateInput(input);
 		}
 
+	}
+	
+	public static void validateInput(String input)
+	{
+		Stack<String> openStack = new Stack<String>();
+		Stack<String> closeStack = new Stack<String>();
+		for ( int j = 0; j < input.length(); j++ ) {
+			String paren = input.substring(j, j+1);
+			if( paren.equals("(") || paren.equals("{") || paren.equals("[") ){
+				openStack.push(paren);
+			} else if( paren.equals(")") ) {
+				closeStack.push("(");
+			} else if ( paren.equals("}") ) {
+				closeStack.push("{");
+			} else if ( paren.equals("]") ) {
+				closeStack.push("[");
+			}else{
+				System.out.println("false");
+				return;
+			}
+		}
+		
+		if( openStack.size() != closeStack.size() ){
+			System.out.println("false");
+			return;
+		}
+		
+		while( !openStack.isEmpty() ){
+			String open = openStack.pop();
+			String close = closeStack.pop();
+			if( !open.equals(close) ){
+				System.out.println("false");
+				return;
+			}
+		}
+		
+		System.out.println("true");
+		return;
 	}
 
 }
