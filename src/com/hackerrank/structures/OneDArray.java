beb2a8838996a0c66ef25b145db6b0e1ad1051d3
Tom Bartolucci <tbartolucci@billtrust.com>
2016-06-01 16:14:25 -0400

    Adding skeleton for two new challenges.

---
+++ src/com/hackerrank/structures/OneDArray.java
@@ -0,0 +1,20 @@
+package com.hackerrank.structures;
+
+import java.util.Scanner;
+
+public class OneDArray {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] numbers = new int[size];
+		int index = 0;
+		while ( sc.hasNext() ){
+			numbers[index] = sc.nextInt();
+			index++;
+		}
+		
+		//int
+	}
+
+}
