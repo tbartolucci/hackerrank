f6d9b961beb7e7aa1f715d163fd1344809d6fb18
Tom Bartolucci <tbartolucci@billtrust.com>
2016-06-02 13:35:23 -0400

    In progress.

--- src/com/hackerrank/structures/OneDArray2.java
+++ src/com/hackerrank/structures/OneDArray2.java
@@ -1,10 +1,44 @@
 package com.hackerrank.structures;
 
+import java.util.Scanner;
+
 public class OneDArray2 {
 
+	/**
+	 * 4
+5 3
+0 0 0 0 0
+6 5
+0 0 0 1 1 1
+6 3
+0 0 1 1 1 0
+3 1
+0 1 0
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		int testCases = sc.nextInt();
+		for ( int i = 0; i < testCases; i++ ) {
+			// Set up input
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int index = 0;
+			int[] arr = new int[n];
+			while ( sc.hasNext() ) {
+				arr[index] = sc.nextInt();
+				index++;
+			}
+			// Solve problem
+			
+			int current = 0;
+			
+			
+			
+			
+			
+		}
 	}
 
 }
