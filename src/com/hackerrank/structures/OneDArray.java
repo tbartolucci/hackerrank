b0bcf36f0eff6d5303beee01baf15db0e6087bca
Tom Bartolucci <tbartolucci@gmail.com>
2016-06-01 21:46:10 -0400

    Moving on to 1D Array Part 2

--- src/com/hackerrank/structures/OneDArray.java
+++ src/com/hackerrank/structures/OneDArray.java
@@ -4,7 +4,22 @@ import java.util.Scanner;
 
 public class OneDArray {
 
+	/**
+	 * 
+	 * 5
+1 -2 4 -5 1
+	 * @param args
+	 */
 	public static void main(String[] args) {
+//
+//		int size = 5;
+//		int[] numbers = new int[size];
+//		numbers[0] = 1;
+//		numbers[1] = -2;
+//		numbers[2] = 4;
+//		numbers[3] = -5;
+//		numbers[4] = 1;
+		
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
 		int[] numbers = new int[size];
@@ -14,7 +29,23 @@ public class OneDArray {
 			index++;
 		}
 		
-		//int
+		int start = 0;
+		int negativeSums = 0;
+		while ( start < size ) {
+			int end = start;
+			while ( end < size ) {
+				int sum = 0;
+				for( int i = start; i <= end; i++){
+					sum += numbers[i];
+				}
+				if( sum < 0 ){
+					negativeSums++;
+				}
+				end++;
+			}
+			start++;
+		}
+		System.out.println(negativeSums);
 	}
 
 }
---
+++ src/com/hackerrank/structures/OneDArray2.java
@@ -0,0 +1,10 @@
+package com.hackerrank.structures;
+
+public class OneDArray2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
