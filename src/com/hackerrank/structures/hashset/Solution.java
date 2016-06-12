23f56dce4f3dc98a741c266741ea1123f19c935c
Tom Bartolucci <tbartolucci@gmail.com>
2016-06-11 22:57:59 -0400

    Started Hashset.

---
+++ src/com/hackerrank/structures/hashset/Solution.java
@@ -0,0 +1,15 @@
+package com.hackerrank.structures.hashset;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+	}
+
+}
