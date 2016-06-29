559be39cd564c6bde36f6d85f84f7fce8172c4b5
tbartolucci <tbartolucci@gmail.com>
2016-06-29 03:46:03 -0400

    Array list test complete.

--- src/com/hackerrank/structures/JavaArrayList.java
+++ src/com/hackerrank/structures/JavaArrayList.java
@@ -13,7 +13,7 @@ public class JavaArrayList {
 		for ( int i = 0; i < n; i++ ) {
 			int d = sc.nextInt();
 			ArrayList<Integer> nums = new ArrayList<Integer>();
-			for ( int j = 0; i < d; j++ ) { 
+			for ( int j = 0; j < d; j++ ) { 
 				nums.add(sc.nextInt());
 			}
 			list.add(nums);
@@ -21,16 +21,29 @@ public class JavaArrayList {
 		
 		int q = sc.nextInt();
 		for ( int i = 0; i < q; i++ ) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			
+			int x = sc.nextInt() - 1;
+			x = ( x < 0 ? 0 : x );
+			int y = sc.nextInt() - 1;
+			y = ( y < 0 ? 0 : y ) ;
+			ArrayList<Integer> tmplist;
+			Integer tmp;
+			System.out.println("Looking up: "+x+" and "+y);
 			try{
-				list.get(x);
+				tmplist = list.get(x);
 			} catch (Exception e ) {
-				
+				System.out.println("ERROR!");
+				continue;
+			}
+			System.out.println(tmplist);	
+			try{
+				tmp = tmplist.get(y);
+			} catch (Exception e ) {
+				System.out.println("ERROR!");
+				continue;
 			}
 			
-			System.out.println(list.get(x).get(y));
+			
+			System.out.println(tmp);
 		}
 		
 		sc.close();
